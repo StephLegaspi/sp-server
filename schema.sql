4139030e8a4975f648772bfb6905a87abf446dfa
StephLegaspi <sylegaspi@up.edu.ph>
2018-10-28 15:58:41 +0800

    Init database schema

---
+++ schema.sql
@@ -0,0 +1,21 @@
+DROP USER IF EXISTS 'transactionserver'@'localhost';
+CREATE USER 'transactionserver'@'localhost' IDENTIFIED BY 'transactionserver';
+
+DROP DATABASE IF EXISTS transactionserver;
+CREATE DATABASE transactionserver;
+
+GRANT ALL PRIVILEGES ON transactionserver.* TO 'transactionserver'@'localhost';
+GRANT EXECUTE ON transactionserver.* TO 'transactionserver'@'localhost';
+
+USE transactionserver;
+
+CREATE TABLE user (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(64),
+    middle_name VARCHAR(64),
+    last_name VARCHAR(64),
+    email_address VARCHAR(64),
+    password VARCHAR(64),
+    contact_number VARCHAR(64)
+);
+
