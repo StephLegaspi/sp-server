0bfd3916e25e1421bad56ca8b0868c10d69d8a99
StephLegaspi <sylegaspi@up.edu.ph>
2019-02-28 14:21:17 +0800

    Old version of master

--- src/entities/authentication/controller.js
+++ src/entities/authentication/controller.js
@@ -3,7 +3,6 @@ const db = require('../../database');
 const bcrypt    = require('bcrypt');
 const salt = bcrypt.genSaltSync(10);
 
-
 exports.login = ( email, password ) => {
   return new Promise((resolve, reject) => {
     const queryString = "SELECT * from user where email_address = '" + email+"'";
