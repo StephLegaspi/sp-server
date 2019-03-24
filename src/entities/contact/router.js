317e1d71102fe8cbde3b0eeecfd2344f6e6073d5
StephLegaspi <sylegaspi@up.edu.ph>
2019-03-24 15:32:32 +0800

    Added initial POST /inquiry

--- src/entities/contact/router.js
+++ src/entities/contact/router.js
@@ -3,8 +3,48 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./controller');
 
+const nodemailer = require("nodemailer");
+
 const async = require('asyncawait/async');
 const await = require('asyncawait/await');
+
+router.post('/inquiry', async (req, res) => {
+  const first_name = req.body.first_name;
+  const middle_name = req.body.middle_name;
+  const last_name = req.body.last_name;
+  const email_address = req.body.email_address;
+  const contact_number = req.body.contact_number;
+  const message = req.body.message;
+
+  const full_name = first_name+' '+middle_name+' '+last_name;
+
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, 
+    auth: {
+      user: 'steph061099@gmail.com',
+      pass: 'twilightsaga1' 
+    }
+  });
+
+  let mailOptions = {
+    from: '"Stephanie Legaspi" <steph061099@gmail.com>', 
+    to: email_address, 
+    subject: "(LJPNCS) Inquiry from: " + full_name, 
+    text: contact_number +'\n'+ message, 
+    html: "<b>" +contact_number +'<br/>'+ message+ "</b>" 
+  };
+ 
+  transporter.sendMail(mailOptions, (error, info) => {
+    try {
+      return console.log('Message %s sent: %s', info.messageId, info.response);
+    } catch (status) {
+      return console.log(error);
+    }  
+  });
+
+});
 
 router.get('/contact_details', async (req, res) => {
   try {
