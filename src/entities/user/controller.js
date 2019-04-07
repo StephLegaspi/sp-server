0d76ba581f850c1e8d0304e0339d7d7f7dab3aae
StephLegaspi <sylegaspi@up.edu.ph>
2019-04-07 22:59:14 +0800

    Added router and controllers for resetting password

--- src/entities/user/controller.js
+++ src/entities/user/controller.js
@@ -3,6 +3,24 @@ const db = require('../../database');
 const bcrypt    = require('bcrypt');
 const salt = bcrypt.genSaltSync(10);
 
+exports.editPassword = (session_id, id, new_password, confirm_password) => {
+  return new Promise((resolve, reject) => {
+
+    if(new_password === confirm_password){
+      bcrypt.hash(new_password, salt, function(err, hash) {
+          const queryString = "CALL changePassword('" + session_id +"', '" + hash +"');";
+
+          db.query(queryString, (err, results) => {
+              if (err) {
+                console.log(err);
+                return reject(500);
+              }
+              return resolve(results);
+          });
+      });
+  }else{ return reject(401); }
+  });
+};
 
 exports.getAll = () =>{
 	return new Promise((resolve, reject) => {
@@ -49,20 +67,3 @@ exports.edit = (session_id, id, first_name, middle_name, last_name, email_addres
     });
 };
 
-exports.editPassword = (session_id, id, new_password, confirm_password) => {
-  return new Promise((resolve, reject) => {
-  	if(new_password === confirm_password){
-	    bcrypt.hash(new_password, salt, function(err, hash) {
-	        const queryString = "CALL changePassword('" + id +"', '" + hash +"');";
-
-	        db.query(queryString, (err, results) => {
-	            if (err) {
-	              console.log(err);
-	              return reject(500);
-	            }
-	            return resolve(results);
-	        });
-	    });
-	}else{ return reject(401); }
-  });
-};
